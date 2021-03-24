959eec1fd129ede8fd0ef58d1f9078a34475fb2e
박상욱 <qkrtkddnr21@gmail.com>
2021-03-24 20:57:44 +0900

    simclr data spliter

--- data/cifar10.py
+++ data/cifar10.py
@@ -94,3 +94,43 @@ class SplitCifar10(CIFAR10):
         assert self.split_idx is not None
 
         return len(self.split_idx)
+
+
+class SplitSimCIFAR10(CIFAR10):
+    def __init__(self,
+                 root: str,
+                 train: bool = True,
+                 transform: Optional[Callable] = None,
+                 target_transform: Optional[Callable] = None,
+                 download: bool = False,
+                 ) -> None:
+        super(SplitSimCIFAR10, self).__init__(root, train, transform, target_transform,
+                                              download)
+        self.split_idx = None
+
+    def set_split(self, split):
+        np_target = np.array(self.targets)
+        split_idxs = np.isin(np_target, split)
+        np_target = np.where(split_idxs == True)[0]
+
+        self.split_idx = np_target
+
+    def __getitem__(self, index) -> Tuple[Any, Any, Any]:
+        assert self.split_idx is not None
+        index = self.split_idx[index]
+
+        img, target = self.data[index], self.targets[index]
+        img = Image.fromarray(img)
+
+        if self.transform is not None:
+            pos_1 = self.transform(img)
+            pos_2 = self.transform(img)
+
+        if self.target_transform is not None:
+            target = self.target_transform(target)
+
+        return pos_1, pos_2, target
+
+    def __len__(self):
+        assert self.split_idx is not None
+        return len(self.split_idx)
